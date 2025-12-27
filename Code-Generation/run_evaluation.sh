acfde0aff2bff48dce76c2145c5597f5af440bed
giordanoDaloisio <giordano.daloisio@gmail.com>
2025-12-27 12:05:07 +0100

    new results distil qwen

--- Code-Generation/run_evaluation.sh
+++ Code-Generation/run_evaluation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -s
 #SBATCH -n 1
-#SBATCH -o ./logs/codegen_eval.out
+#SBATCH -o ./logs/codegen_eval_%j.out
 #SBATCH -J codegen
 #SBATCH -p normal
 #SBATCH -c 20
@@ -16,4 +16,4 @@ export NUMEXPR_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 source /NFSHOME/gdaloisio/miniconda3/etc/profile.d/conda.sh
 conda activate codex
 
-srun python evaluate_samples.py --sample_file samples_500369.jsonl
+srun python evaluate_samples.py --sample_file samples_500384.jsonl
